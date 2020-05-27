e6e1ad220951aab004d27c3818c19f6beb3cdeb5
Stanley <stanleygomesdasilva@gmail.com>
2020-05-26 22:27:46 -0300

    error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.website')
+@section('pageTitle', 'Página inicial')
+
+@section('content')
+
+<div class="container pt-5 pb-5">
+    <div class="row pt-5 pb-5">
+        <div class="col-sm-12 pt-5 pb-5 text-center">
+            <h1>
+                <i class="fa fa-frown-open"></i>
+                <div>
+                    Erro 404.
+                </div>
+            </h1>
+            <p>A página que você procurou não foi encontrada.</p>
+        </div>
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.website')
+@section('pageTitle', 'Página inicial')
+
+@section('content')
+
+<div class="container pt-5 pb-5">
+    <div class="row pt-5 pb-5">
+        <div class="col-sm-12 pt-5 pb-5 text-center">
+            <h1>
+                <i class="fa fa-frown-open"></i>
+                <div>
+                    Erro 500.
+                </div>
+            </h1>
+            <p>Ocorreu um erro em nosso site.</p>
+        </div>
+    </div>
+</div>
+
+@endsection
