9159b8ada685a260a5d1db13ab5206e140e7156a
Stanley <stanleygomesdasilva@gmail.com>
2020-06-11 11:32:03 -0300

    transaction fix

--- server/migrations/V001__init_db.sql
+++ server/migrations/V001__init_db.sql
@@ -248,23 +248,9 @@ CREATE TABLE `contact_reply` (
 
 CREATE TABLE `transaction` (
     `id` int(10) NOT NULL AUTO_INCREMENT,
-    `customer_id` int(10) NOT NULL,
-    `qty` int(10) NOT NULL,
-    `payment_method_id` int(10) NOT NULL,
-    `total_price` float(8,2) NOT NULL,
-    `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at` timestamp NULL DEFAULT NULL,
-    `created_by` int(10) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
-
-CREATE TABLE `transaction_history` (
-    `id` int(10) NOT NULL AUTO_INCREMENT,
-    `transaction_id` int(10) NOT NULL,
-    `status` varchar(255) NOT NULL,
-    `status_detail` varchar(255) NOT NULL,
-    `message` varchar(255) NOT NULL,
+    `cart_id` int(10) NOT NULL,
+    `preference_id` varchar(255) NOT NULL,
+    `preference_url` varchar(255) NOT NULL,
     `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
     `deleted_at` timestamp NULL DEFAULT NULL,
@@ -346,11 +332,11 @@ ALTER TABLE `user_password_reset` ADD FOREIGN KEY (`created_by`) REFERENCES `use
 ALTER TABLE `user_login_history` ADD FOREIGN KEY (`created_by`) REFERENCES `user` (`id`);
 ALTER TABLE `user_login_history` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 ALTER TABLE `user` ADD FOREIGN KEY (`created_by`) REFERENCES `user` (`id`);
-ALTER TABLE `transaction_history` ADD FOREIGN KEY (`transaction_id`) REFERENCES `transaction` (`id`);
-ALTER TABLE `transaction` ADD FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
 ALTER TABLE `store` ADD FOREIGN KEY (`created_by`) REFERENCES `user` (`id`);
 ALTER TABLE `wishlist_item` ADD FOREIGN KEY (`created_by`) REFERENCES `user` (`id`);
 ALTER TABLE `contact` ADD FOREIGN KEY (`created_by`) REFERENCES `user` (`id`);
 ALTER TABLE `contact_reply` ADD FOREIGN KEY (`created_by`) REFERENCES `user` (`id`);
+ALTER TABLE `transaction` ADD FOREIGN KEY (`cart_id`) REFERENCES `cart` (`id`);
+ALTER TABLE `transaction` ADD FOREIGN KEY (`created_by`) REFERENCES `user` (`id`);
 
 CREATE UNIQUE INDEX user_email_unique ON `user` (`email`);
